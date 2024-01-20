9a1d9db7855ac0d644b6640f54f89767ec7efd40
Vishal2109 <137199815+Vishal2109@users.noreply.github.com>
2024-01-20 12:54:56 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+
+//        rwOnly.name = "sdf";
+//        java: name has private access in com.driver.RWOnly
+
+//        System.out.println(rwOnly.name);
+//        java: name has private access in com.driver.RWOnly
+
+        rwOnly.setName("vishal");
+        System.out.println(rwOnly.getName());
+    }
 }
